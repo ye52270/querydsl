fe6667034e8da31951eb9e2af5c7df0d31001eff
Park Jae Young <71998296+ye52270@users.noreply.github.com>
2024-04-22 19:20:16 +0900

    1

--- src/main/java/hello/querydsl/QuerydslApplication.java
+++ src/main/java/hello/querydsl/QuerydslApplication.java
@@ -1,6 +1,5 @@
 package hello.querydsl;
 
-import hello.querydsl.entity.QMember;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
